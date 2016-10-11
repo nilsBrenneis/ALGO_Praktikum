b135066b9e21e5ab09851b2f8b2bb219ca3b39da
nils <nils@nils-ThinkPad-X260>
2016-10-11 17:19:23 +0200

    save

--- src/nbcn/termin1/aufgabe1/Friend.java
+++ src/nbcn/termin1/aufgabe1/Friend.java
@@ -7,16 +7,54 @@ public class Friend {
 	private int id;
 	private String name;
 	private String surname;
-	private ArrayList<Integer> friends;
-	
+	private ArrayList<Integer> friendsArrLi;
+
 	Friend(int id, String name, String surname) {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
-		friends = new ArrayList<Integer>();
+		friendsArrLi = new ArrayList<Integer>();
 	}
 	
 	public void addFriends(int id) {
-		friends.add(id);
+		friendsArrLi.add(id);
 	}
+	
+	public int[] getFriends() {
+		int[] friendsArr = new int[friendsArrLi.size()];
+		for (int i = 0; i < friendsArr.length; i++) {
+			friendsArr[i] = friendsArrLi.get(i);
+		}
+		return friendsArr;
+	}
+	
+	public int getFriendsCount() {
+		return friendsArrLi.size();
+	}	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	
 }
--- src/nbcn/termin1/aufgabe1/Runner.java
+++ src/nbcn/termin1/aufgabe1/Runner.java
@@ -4,31 +4,63 @@ import java.util.ArrayList;
 
 public class Runner {
 
-	private static final int N_FREUNDE = 5;
 	private static final int K_FREUNDE = 2;
-	private ArrayList<Friend> friendsToInv;
+	private static ArrayList<Friend> friendsToInv = new ArrayList<Friend>();
 
 	private void fillFriendsList() {
 		friendsToInv.add(new Friend(0, "Peters", "Peter"));
-		friendsToInv.add(new Friend(1, "Müller", "Meike"));
+		friendsToInv.add(new Friend(1, "Spex", "Richtael"));
 		friendsToInv.add(new Friend(2, "Meyer", "Michael"));
 		friendsToInv.add(new Friend(3, "Mustermann", "Max"));
 		friendsToInv.add(new Friend(4, "Musterfrau", "Erika"));
-		
-		
+
 		friendsToInv.get(0).addFriends(4);
 		friendsToInv.get(0).addFriends(3);
-		
+
 		friendsToInv.get(1).addFriends(0);
 		friendsToInv.get(1).addFriends(5);
-		
+
 		friendsToInv.get(2).addFriends(1);
 		friendsToInv.get(2).addFriends(4);
-		
+
 		friendsToInv.get(3).addFriends(0);
 		friendsToInv.get(3).addFriends(4);
-		
+
 		friendsToInv.get(4).addFriends(3);
 		friendsToInv.get(4).addFriends(5);
+	}
+
+	private void siftList() {
+		for (Friend friend : friendsToInv) {
+			int friendsInList = 0;
+			for (int friend_id : friend.getFriends()) {
+				if (containsId(friend_id)) {
+					friendsInList++;
+				}
+			}
+			if (friendsInList < K_FREUNDE) {
+				friendsToInv.remove(friend);
+				siftList();
+			}
+		}
+	}
+
+	private boolean containsId(int id) {
+		for (Friend friend : friendsToInv) {
+			if (friend.getId() == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Runner run = new Runner();
+		run.fillFriendsList();
+		run.siftList();
+		for (Friend friend : friendsToInv) {
+			System.out.print(friend.getId() + " " + friend.getSurname() + ", ");
+		}
+		
 	}
 }
