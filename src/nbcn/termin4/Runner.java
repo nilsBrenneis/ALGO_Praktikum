051cb2725ac85c0ff78e16cac8d243247a0da3f1
Nils <nilsstnurfuersam@gmail.com>
2016-12-14 10:33:45 +0100

    Test of IntelliJ's git functionality

---
+++ src/nbcn/termin4/Runner.java
@@ -0,0 +1,11 @@
+package nbcn.termin4;
+
+/**
+ * Created by nils on 14.12.16.
+ */
+public class Runner {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
