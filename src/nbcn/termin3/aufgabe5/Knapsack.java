1c7efe697b9a18498a8c23a7237f23f8ec11528f
Nils Brenneis <nilsstnurfuersam@gmail.com>
2019-03-23 13:11:05 +0100

    fix output

--- src/nbcn/termin3/aufgabe5/Knapsack.java
+++ src/nbcn/termin3/aufgabe5/Knapsack.java
@@ -25,7 +25,7 @@ public class Knapsack {
 
 	private static void printTable(int[][] table) {
 		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j <= table.length; j++) {
+			for (int j = 0; j < table[i].length; j++) {
 				System.out.print("[" + table[i][j] + "]" + " ");
 			}
 			System.out.println(System.lineSeparator());
